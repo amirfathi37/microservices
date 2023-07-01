c23086e30ebe7027afdc1bfc842f7db797deffd9
amirfathi37 <fathiamir37@gmail.com>
2023-07-01 16:58:58 +0330

    feat(circute-breaker): resilience4J

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,8 +18,16 @@ public class OrderController {
 
     @PostMapping(value = "/place-order")
     @ResponseBody
+    @CircuitBreaker(name = "inventory", fallbackMethod = "inventoryFallbackMethod")
+//    @TimeLimiter(name = "inventory")
+//    @Retry(name = "inventory")
     public ResponseEntity<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
-        return ResponseEntity.ok("{'message': 'The Order Has been Placed!!!'}");
+        return ResponseEntity.ok("{'m essage': 'The Order Has been Placed!!!'}");
+    }
+
+    public ResponseEntity<String> inventoryFallbackMethod(OrderRequest orderRequest, RuntimeException e) {
+        return (ResponseEntity<String>) ResponseEntity
+                .internalServerError();
     }
 }
